be9ea20f969e1a1ea295ccd944315e7c7bd43191
PJ <pj@PJs-MacBook-Air.lan>
2021-04-28 00:21:03 +0800

    Add Classification model

---
+++ classification.py
@@ -0,0 +1,134 @@
+# %%
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+# %%
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+validation_split = 0.1
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+
+trainset_len = len(trainset)
+indices = list(range(trainset_len))
+
+# Randomly split indices
+val_len = int(np.floor(validation_split * trainset_len))
+val_idx = np.random.choice(indices, size=val_len, replace=False)
+train_idx = list(set(indices) - set(val_idx))
+
+print(val_idx)
+validset = [trainset[i] for i in val_idx]
+trainset = [trainset[i] for i in train_idx]
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+valloader = torch.utils.data.DataLoader(validset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+
+## Defining the samplers for each phase based on the random indices:
+# train_sampler = SubsetRandomSampler(train_idx)
+# validation_sampler = SubsetRandomSampler(validation_idx)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# %%
+import matplotlib.pyplot as plt
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+# %%
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+# %%
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# %%
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    val_running_loss = 0
+    for j, data in enumerate(valloader, 0):
+        inputs, labels = data
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        val_running_loss += loss.item()
+    print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / len(valloader)))
+
+        
+         
+print('Finished Training')# %%
